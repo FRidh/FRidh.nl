f8128b46723f317620770688338409ae86f10ae2
Frederik Rietdijk <fridh@fridh.nl>
2018-03-11 14:02:37 +0100

    update nix expr

--- shell.nix
+++ shell.nix
@@ -1,9 +1,3 @@
 with import <nixpkgs> {};
-{
-  envname = stdenv.mkDerivation {
-    name = "envname";
 
-    propagatedBuildInputs = with pkgs.python27Packages; [ ghp-import pelican Fabric ipython markdown jupyter];
-  };
-
-}
+(python3.withPackages(ps: with ps; [ghp-import pelican notebook markdown ])).env
